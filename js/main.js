db48dbc5aa5239fa2cc47b4ecf6ae1b30f4c7bfe
gcooper5 <gcooper5@wisc.edu>
2016-03-04 15:04:06 -0600

    week-5 commit
    5th operator is map overlay

--- js/main.js
+++ js/main.js
@@ -64,47 +64,55 @@
 
 // map.on('click', onMapClick);
 
-
+//the links to the alternate map tile and attribution
+var streets =    L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+}),
+night = L.tileLayer('http://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	maxZoom: 19
+});
 
 //function to instantiate the Leaflet map
 function createMap(){
-    //create the map
+    //create the map, and adding the layer options
     var map = L.map('map', {
         center: [44, 14],
-        zoom: 5
+        zoom: 5,
+        layer:[night]
     });
 
-    //pulling in the map box tile layer and setting the max zoom
-
-    L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
-	maxZoom: 16
-	}).addTo(map);
-
+    //defining the other base maps to switch too
+    var baseMaps = {
+        "Night": night,
+        "Streets": streets
+    };
+L.control.layers(baseMaps).addTo(map);
+//setting the initial base map
+L.tileLayer('http://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	maxZoom: 19
+}).addTo(map);
     //call getData function
     getData(map);
 };
+//create Sequence control buttons and range slider function, takes the values map and attributes
 function createSequenceControls(map, attributes){
+  // creates the variable sequence control layer
   var SequenceControl = L.Control.extend({
-    options:{
-      posistion: 'bottomleft'
-    },
+//creating the dive container for the sequence contol buttons on the map and adding it to the map
     onAdd: function(map){
       var container = L.DomUtil.create('div', 'sequence-control-container');
       $(container).append('<input class="range-slider" type="range">');
 
-//add skip buttons
+//add skip buttons both forward and reverse- styling is in the css
 $(container).append('<button class="skip" id="reverse" title="Reverse">Reverse</button>');
 $(container).append('<button class="skip" id="forward" title="Forward">Skip</button>');
 
-//kill any mouse event listeners on the map
-$(container).on('mousedown dblclick', function(e){
-  L.DomEvent.stopPropagation(e);
-});
 return container;
     }
   });
-//  $("#slider").append('<input class="range-slider" type="range">');
+//defining the range control on the slider bar for the number of years to scroll through
 map.addControl(new SequenceControl());
  $('.range-slider').attr({
    max: 11,
@@ -144,7 +152,7 @@ function pointToLayer(feature, latlng, attributes){
   var attribute = attributes[0];
   var geojsonMarkerOptions = {
       radius: 9,
-      fillColor: "#662441",
+      fillColor: "#ff00bf",
       color: "#662441",
       weight: 1,
       opacity: 1,
@@ -157,7 +165,7 @@ function pointToLayer(feature, latlng, attributes){
   geojsonMarkerOptions.radius = calcPropRadius(attValue);
 
   //logging to the console the features and attributes
-      //  console.log(feature.properties, attValue);
+       console.log(feature.properties, attValue);
 
 // this creates the layer with the proportional symbols- places them with the lat and lon
   var layer = L.circleMarker(latlng, geojsonMarkerOptions);
@@ -188,13 +196,14 @@ function pointToLayer(feature, latlng, attributes){
 
   return layer;
 };
+//updating the proportional symbols with the slider bar based on what year it is
 function updatePropSymbols(map, attribute){
     map.eachLayer(function(layer){
         if (layer.feature && layer.feature.properties[attribute]){
             var props = layer.feature.properties;
             var radius = calcPropRadius(props[attribute]);
             layer.setRadius(radius);
-
+//setting the popup content for each country, updating as the year scrolls through
             var popupContent = "<p><b>Country: " + props.Country + "</p>";
             var year = attribute.split("_")[1];
             popupContent += "</p><p><b>Women in Government "+ attribute + ":</b> " + props[attribute] + " %  </p>";
@@ -210,13 +219,13 @@ function updatePropSymbols(map, attribute){
 //scaling each circle marker as a proportional symbol
 function calcPropRadius(attValue) {
   //the factor to scale each symbol by
-  var scaleFactor = 60;
+  var scaleFactor = 90;
   var area = attValue * scaleFactor;
   var radius = Math.sqrt(area/Math.PI);
 
   return radius;
 };
-
+// calling through each year of the geojson data
 function processData(data){
   var attributes=[];
   var properties=data.features[0].properties;
@@ -226,10 +235,10 @@ function processData(data){
       attributes.push(attribute);
     };
   };
-  // console.log(attributes);
   return attributes;
 };
 
+//defining the proportional symbols to express the data- will be manipulated above by year
 function createPropSymbols(data, map, attributes){
 //creating a geojson layer too add the proportional symbols to the Map
   L.geoJson(data, {
@@ -248,6 +257,7 @@ function getData(map){
           var attributes = processData(response);
 //calling the createProportional symbol function
           createPropSymbols(response, map, attributes);
+//calling the create sequence control           
           createSequenceControls(map, attributes);
         }
     })
